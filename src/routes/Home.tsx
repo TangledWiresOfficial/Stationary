b8f38718f5bbf3c399c3befdd96657997524b4fb
xWires <xwires@tangledwires.xyz>
2025-12-03 15:43:10 +0000

    Show which lines have been visited

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,11 @@
 import {PageHeader} from "../components/PageHeader.tsx";
-import {EmptyState, EmptyStateBody, PageSection} from "@patternfly/react-core";
+import {EmptyState, EmptyStateBody, Flex, FlexItem, PageSection} from "@patternfly/react-core";
 import {ChartDonut, ChartLabel} from "@patternfly/react-charts/victory";
-import {VictoryStyleInterface} from "victory-core";
 import {LineId, Lines} from "../utils/line.ts";
 import {useVisitsPerLine} from "../hooks/useVisitsPerLine.ts";
 import {useJourneys} from "../hooks/useJourneys.ts";
 import CubesIcon from '@patternfly/react-icons/dist/esm/icons/cubes-icon';
 import BarsIcon from '@patternfly/react-icons/dist/esm/icons/bars-icon';
-
-const stationChartStyle: VictoryStyleInterface = {
-  data: {
-    fill: ({ datum }) => Lines[datum.x as LineId].colour,
-  },
-};
 
 export function Home() {
   const { journeys, loading } = useJourneys();
@@ -25,27 +18,59 @@ export function Home() {
         {journeys.length > 0 ? (
           <>
             {visitsPerLine && (
-              <div style={{ height: '150px', width: '150px' }}>
-                <ChartDonut
-                  constrainToVisibleArea
-                  style={stationChartStyle}
-                  labels={({ datum }) => `${Lines[datum.x as LineId].displayName}: ${datum.y}`}
-                  height={150}
-                  width={150}
-                  title={`Journeys\nper line`}
-                  titleComponent={
-                    <ChartLabel style={[{
-                      fontSize: 16
-                    }]} />
-                  }
-                  data={Object.entries(visitsPerLine).map(([line, visits]) => {
-                    return {
-                      x: line,
-                      y: visits
-                    };
-                  })}
-                />
-              </div>
+              <Flex>
+                <FlexItem style={{ height: '150px', width: '150px' }}>
+                  <ChartDonut
+                    constrainToVisibleArea
+                    style={{
+                      data: {
+                        fill: ({ datum }) => Lines[datum.x as LineId].colour,
+                      },
+                    }}
+                    labels={({ datum }) => `${Lines[datum.x as LineId].displayName}: ${datum.y}`}
+                    height={150}
+                    width={150}
+                    title={`Journeys\nper line`}
+                    titleComponent={
+                      <ChartLabel style={[{
+                        fontSize: 16
+                      }]} />
+                    }
+                    data={Object.entries(visitsPerLine).map(([line, visits]) => {
+                      return {
+                        x: line,
+                        y: visits
+                      };
+                    })}
+                  />
+                </FlexItem>
+                <FlexItem style={{ height: '150px', width: '150px' }}>
+                  <ChartDonut
+                    constrainToVisibleArea
+                    style={{
+                      data: {
+                        fill: ({ datum }) => datum.visits > 0 ? Lines[datum.x as LineId].colour : "var(--pf-t--color--gray--20)",
+                      },
+                    }}
+                    labels={({ datum }) => Lines[datum.x as LineId].displayName}
+                    height={150}
+                    width={150}
+                    title={`Lines\nvisited`}
+                    titleComponent={
+                      <ChartLabel style={[{
+                        fontSize: 16
+                      }]} />
+                    }
+                    data={Object.entries(visitsPerLine).map(([line, visits]) => {
+                      return {
+                        x: line,
+                        y: 1,
+                        visits: visits
+                      };
+                    })}
+                  />
+                </FlexItem>
+              </Flex>
             )}
           </>
         ) : !loading && (
