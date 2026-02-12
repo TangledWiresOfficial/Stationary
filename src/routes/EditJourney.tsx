05f99a7ad610637acc33184d5c7c06fa278a5714
xWires <xwires@tangledwires.xyz>
2026-02-12 15:45:54 +0000

    Redirect to journey history page if trying to edit a journey that doesn't exist

--- src/routes/EditJourney.tsx
+++ src/routes/EditJourney.tsx
@@ -1,18 +1,26 @@
 import {PageHeader} from "../components/PageHeader.tsx";
-import {useParams} from "react-router";
+import {useNavigate, useParams} from "react-router";
 import {useJourneys} from "../hooks/useJourneys.ts";
 import {JourneyWizard} from "../components/JourneyWizard.tsx";
+import {Journey} from "../utils/journey.ts";
 
 export function EditJourney() {
+  const navigate = useNavigate();
   const params = useParams();
 
   const { journeys, loading } = useJourneys();
+  let journey: Journey | undefined;
+
+  if (!loading) {
+    journey = journeys.find((j) => j.uuid === params.uuid);
+    if (!journey) navigate("/journeyhistory")
+  }
 
   return (
     <>
       <PageHeader title="Edit journey" />
       {!loading && (
-        <JourneyWizard initialJourney={journeys.find((j) => j.uuid === params.uuid)} />
+        <JourneyWizard initialJourney={journey} />
       )}
     </>
   );
