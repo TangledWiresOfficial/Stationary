71b4ae4627c2c5479f371574e4cc606afef61edf
xWires <xwires@tangledwires.xyz>
2026-04-26 17:18:42 +0100

    Start working on deep linking

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,13 @@ import "@patternfly/react-core/dist/styles/base.css";
 import "@saurl/tauri-plugin-safe-area-insets-css-api";
 import "./App.css";
 
+import {getCurrent} from "@tauri-apps/plugin-deep-link";
+
+const startUrls = await getCurrent();
+if (startUrls) {
+  window.location.href = startUrls[0].replace(/stationary:\/$/, "")
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
